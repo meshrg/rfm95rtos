f2ce5d3db0ad7d119f49a3ee7f589330a6df5e15
Fernando Valderrabano <fdo.valderrabano@gmail.com>
2024-03-23 19:42:54 -0700

    RFM95-1,  feat: Initialize radio via SPI
    * added a condition to test if radio is connected thru SPI bus.
    * added a function to read SPI register.
    * added a function to hard reset radio through the reset pin.

--- main/main.c
+++ main/main.c
@@ -1,65 +1,120 @@
-#include <stdio.h>
-#include <string.h>
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/spi_master.h"
-#include "driver/gpio.h"
 #include "esp_log.h"
+#include <string.h>
+#include <driver/gpio.h>
+#include "driver/spi_master.h"
 
-// pins to use, please refer to
-// variants/esp32/pins_arduino.h
+// SPI3_HOST
+#define GPIO_NUM_MOSI 			23
+#define GPIO_NUM_MISO 			19
+#define GPIO_NUM_SCLK 			18
+#define GPIO_NUM_CS    			 5
 
-// HSPI
-// #define GPIO_MISO 12
-// #define GPIO_MOSI 13
-// #define GPIO_SCLK 14
-// #define GPIO_CS   15
+#define GPIO_NUM_RST  			13
+#define GPIO_NUM_G0   			27
+#define TAG 		  			"RFM9x"
+#define TIMEOUT_RESET			100
 
-// VSPI
-#define GPIO_MOSI 23
-#define GPIO_MISO 19
-#define GPIO_SCLK 18
-#define GPIO_CS   05
+// This is the bit in the SPI address that marks it as a write
+#define RH_RF95_REG_VERSION      	0x42
+#define RH_FIFO_TX_BASE_ADDR        0x0e
+#define RH_FIFO_RX_BASE_ADDR        0x0f
 
-#define CLOCK_SPEED_HZ 10
+// function prototypes
+esp_err_t spi_init(void);
+void reset_radio(void);
+int register_read(int reg);
 
-static const char TAG[] = "VSPI";
+static spi_device_handle_t spi_handle;
 
 void app_main(void)
 {
-    spi_bus_config_t buscfg = {
-        .mosi_io_num   = GPIO_MOSI,
-        .miso_io_num   = GPIO_MISO,
-        .sclk_io_num   = GPIO_SCLK
+	uint8_t version_register;
+	uint8_t result;
+
+	spi_init();
+	reset_radio();
+
+	version_register = RH_RF95_REG_VERSION;
+	printf("Reading version register: %x\n", version_register);
+	result = register_read(version_register);
+	printf("fetched result is: 0x%x = %i\n", result, result);
+	if (result == 0x12) {
+		printf("LoRa radio init success\n");
+	} else{
+		printf("LoRa radio init failed. Check wiring.\n");
+	}
+
+}
+
+esp_err_t spi_init(void)
+{
+	esp_err_t ret;
+
+	// reset pin configuration
+	gpio_reset_pin(GPIO_NUM_RST);
+   	gpio_set_direction(GPIO_NUM_RST, GPIO_MODE_OUTPUT);
+
+	// chip select pin configuration
+   	gpio_reset_pin(GPIO_NUM_CS);
+   	gpio_set_direction(GPIO_NUM_CS, GPIO_MODE_OUTPUT);
+	gpio_set_level(GPIO_NUM_CS, 1);
+
+	spi_bus_config_t spi_bus_config = {
+        .miso_io_num = GPIO_NUM_MISO,
+        .mosi_io_num = GPIO_NUM_MOSI,
+        .sclk_io_num = GPIO_NUM_SCLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+		.max_transfer_sz = 0
     };
 
-    spi_device_interface_config_t devcfg = {
-        .mode           = 0,
-        .spics_io_num   = GPIO_CS,
-        .clock_speed_hz = CLOCK_SPEED_HZ * 1000 * 1000,
-        .queue_size     = 1
-    };
+	ret = spi_bus_initialize(SPI3_HOST, &spi_bus_config, SPI_DMA_CH_AUTO);
+	ESP_LOGI(TAG, "spi_bus_initialize=%d",ret);
+	assert(ret == ESP_OK);
 
-    // spi initialization
-    spi_bus_initialize(VSPI_HOST, &buscfg, 1);
+	spi_device_interface_config_t spi_device_interface_config = {
+		.clock_speed_hz = 5 * 1000 * 1000,
+		.mode = 0,  // CPOL=0, CPHA=0
+		.queue_size = 1,
+		.spics_io_num = -1,
+		// .flags = SPI_DEVICE_NO_DUMMY,
+		.pre_cb = NULL,
+		.post_cb = NULL
+	};
 
-    // spi handle
-    spi_device_handle_t spi_handle;
-    spi_bus_add_device(VSPI_HOST, &devcfg, &spi_handle);
+	ret = spi_bus_add_device(SPI3_HOST, &spi_device_interface_config, &spi_handle);
+	ESP_LOGI(TAG, "spi_bus_add_device=%d", ret);
+	assert(ret == ESP_OK);
 
-    // spi transaction
-    spi_transaction_t transaction;
-    memset(&transaction, 0, sizeof(transaction));
-    transaction.length = 20;
+	return ESP_OK;
+}
 
-    const char test_str[] = "A"; // 01000001
-    transaction.tx_buffer = test_str;
+void reset_radio(void)
+{
+	ESP_LOGI(TAG, "reseting lora radio. Starting...");
+	gpio_set_level(GPIO_NUM_RST, 0);
+	vTaskDelay(pdMS_TO_TICKS(1));
+	gpio_set_level(GPIO_NUM_RST, 1);
+	vTaskDelay(pdMS_TO_TICKS(1));
+	ESP_LOGI(TAG, "reseting lora radio. Done.");
+}
 
-    while (1)
-    {
-        ESP_LOGI(TAG, "Sending char A");
-        spi_device_transmit(spi_handle, &transaction);
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-    }
+
+int register_read(int reg)
+{
+   uint8_t buffer_out[2] = { reg, 0xff };
+   uint8_t buffer_in[2];
+
+   spi_transaction_t t = {
+      .flags = 0,	// transaction line mode
+      .length = 8 * sizeof(buffer_out),
+      .tx_buffer = buffer_out,
+      .rx_buffer = buffer_in
+   };
+
+   gpio_set_level(GPIO_NUM_CS, 0);
+   spi_device_transmit(spi_handle, &t);
+   gpio_set_level(GPIO_NUM_CS, 1);
+
+   return buffer_in[1];
 }
